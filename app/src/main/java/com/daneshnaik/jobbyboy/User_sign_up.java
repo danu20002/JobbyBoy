8c8b445f05a22c3b7426a2fc68236918f1b55dc7
Danesh Naik <naikdanesh2@gmail.com>
2023-10-31 00:05:50 +0530

    Updated now

---
+++ app/src/main/java/com/daneshnaik/jobbyboy/User_sign_up.java
@@ -0,0 +1,14 @@
+package com.daneshnaik.jobbyboy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_sign_up extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_sign_up);
+    }
+}
