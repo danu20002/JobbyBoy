d411b77862682e8b54f01bbcac0dd8bb3b5954d3
Danesh Naik <naikdanesh2@gmail.com>
2023-11-01 01:21:30 +0530

    Updated now

--- app/src/main/java/com/daneshnaik/jobbyboy/activities/mainscreen.java
+++ app/src/main/java/com/daneshnaik/jobbyboy/activities/mainscreen.java
@@ -17,7 +17,6 @@ public class mainscreen extends AppCompatActivity {
         bottomNavigation.show(1,true);
         bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.baseline_home_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.baseline_recommend_24));
-
         bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.baseline_chat_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(5,R.drawable.baseline_person_4_24));
     }
