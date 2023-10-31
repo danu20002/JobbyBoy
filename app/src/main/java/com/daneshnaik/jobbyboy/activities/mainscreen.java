344a3aa4ee5076ae7a18f2d220313050e48ed04a
Danesh Naik <naikdanesh2@gmail.com>
2023-10-31 16:25:17 +0530

    Updated now

--- app/src/main/java/com/daneshnaik/jobbyboy/activities/mainscreen.java
+++ app/src/main/java/com/daneshnaik/jobbyboy/activities/mainscreen.java
@@ -5,12 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.daneshnaik.jobbyboy.R;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class mainscreen extends AppCompatActivity {
-
+    MeowBottomNavigation bottomNavigation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
+        bottomNavigation=findViewById(R.id.meowbottomnav);
+        bottomNavigation.show(1,true);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.baseline_recommend_24));
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(4,R.drawable.baseline_person_4_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(5,R.drawable.baseline_person_4_24));
     }
 }
