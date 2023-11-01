add64969925272f2000c3139ad1c36e3e207ed24
Danesh Naik <naikdanesh2@gmail.com>
2023-11-01 10:02:33 +0530

    Updated now

--- app/src/main/java/com/daneshnaik/jobbyboy/activities/Recommend.java
+++ app/src/main/java/com/daneshnaik/jobbyboy/activities/Recommend.java
@@ -2,15 +2,88 @@ package com.daneshnaik.jobbyboy.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.daneshnaik.jobbyboy.R;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
 
 public class Recommend extends AppCompatActivity {
-
+    MeowBottomNavigation bottomNavigation_recommned;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recommend);
+        bottomNavigation_recommned=findViewById(R.id.meowbottomnav_recommend);
+
+
+
+        bottomNavigation_recommned.show(2,true);
+        bottomNavigation_recommned.add(new MeowBottomNavigation.Model(1,R.drawable.baseline_home_24));
+        bottomNavigation_recommned.add(new MeowBottomNavigation.Model(2,R.drawable.baseline_recommend_24));
+        bottomNavigation_recommned.add(new MeowBottomNavigation.Model(4,R.drawable.baseline_chat_24));
+        bottomNavigation_recommned.add(new MeowBottomNavigation.Model(5,R.drawable.baseline_person_4_24));
+        bottomNavigation_recommned.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                if (model.getId() == 1) {
+                    startActivity(new Intent(getApplicationContext(), mainscreen.class));
+                    finish();
+                } else if (model.getId() == 2) {
+                    startActivity(new Intent(getApplicationContext(), Recommend.class));
+                } else if (model.getId() == 3) {
+                    startActivity(new Intent(getApplicationContext(), Chat.class));
+                    finish();
+                } else if (model.getId() == 4) {
+                    startActivity(new Intent(getApplicationContext(), profile.class));
+                    finish();
+                }
+                return null;
+            }
+        });
+        bottomNavigation_recommned.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                if (model.getId() == 1) {
+                    startActivity(new Intent(getApplicationContext(), mainscreen.class));
+                    finish();
+                }
+                return null;
+            }
+        });
+        bottomNavigation_recommned.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                if (model.getId() == 2) {
+                    startActivity(new Intent(getApplicationContext(), Recommend.class));
+                    finish();
+                }
+                return null;
+            }
+        });
+        bottomNavigation_recommned.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                if (model.getId() == 3) {
+                    startActivity(new Intent(getApplicationContext(), Chat.class));
+                    finish();
+                }
+                return null;
+            }
+        });
+        bottomNavigation_recommned.setOnShowListener(new Function1<MeowBottomNavigation.Model, Unit>() {
+            @Override
+            public Unit invoke(MeowBottomNavigation.Model model) {
+                if (model.getId() == 4) {
+                    startActivity(new Intent(getApplicationContext(), profile.class));
+                    finish();
+                }
+                return null;
+            }
+        });
+
     }
 }
--- app/src/main/java/com/daneshnaik/jobbyboy/activities/profile.java
+++ app/src/main/java/com/daneshnaik/jobbyboy/activities/profile.java
@@ -5,12 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.daneshnaik.jobbyboy.R;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class profile extends AppCompatActivity {
-
+    MeowBottomNavigation bottomNavigation_profile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        bottomNavigation_profile=findViewById(R.id.meowbottomnav_profile);
+        bottomNavigation_profile.show(1,true);
+        bottomNavigation_profile.add(new MeowBottomNavigation.Model(1,R.drawable.baseline_home_24));
+        bottomNavigation_profile.add(new MeowBottomNavigation.Model(2,R.drawable.baseline_recommend_24));
+        bottomNavigation_profile.add(new MeowBottomNavigation.Model(4,R.drawable.baseline_chat_24));
+        bottomNavigation_profile.add(new MeowBottomNavigation.Model(5,R.drawable.baseline_person_4_24));
     }
 }
